6d257e0b6fa613b68cc25e725ae22b655ced9ce1
devarajshekar@gmail.com <devarajshekar@gmail.com>
2024-04-14 10:47:55 +0530

    hello first

---
+++ structure.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+typedef struct
+    {
+    int roll;
+    char a[50];
+    float perc;
+        }student;
+void main()
+{
+student st[2],*p;
+p=st;
+printf("enter student detrails");
+for(int i=0;i<2;i++)
+{
+    scanf("%d%s%f",&p->roll,p->a,&p->perc);
+    p++;
+}
+printf("student details are");
+p=st;
+for(int i=0;i<2;i++)
+{
+    printf("\t%d\t%s\t%f",p->roll,p->a,p->perc);
+    p++;
+}
+
+
+
+}
